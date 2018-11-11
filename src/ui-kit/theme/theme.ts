6f3947d4cb8e77918ae4a36c0b63cd8db1bed13d
Mate Papp <mate.papp@cogito.study>
2018-11-11 20:31:15 +0100

    Create new theme object

--- src/ui-kit/theme/theme.ts
+++ src/ui-kit/theme/theme.ts
@@ -1,16 +1,27 @@
 export const theme = {
-  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
-  colors: {
-    blue: '#07c',
-    lightgray: '#f6f6ff',
+  global: {
+    colors: {
+      primary: '#4787D3',
+      gradient: 'linear-gradient(306.87deg, #4787D3 0%, #67BCFB 100%)',
+      light: '#FBFBFB',
+      dark: '#101010',
+      grey: '#595959',
+      lightGrey: '#9C9C9C',
+    },
+    font: {
+      family: 'Montserrat',
+    },
   },
-  space: [0, 4, 8, 16, 32, 64, 128, 256],
-  fonts: {
-    sans: 'system-ui, sans-serif',
-    mono: 'Montserrat, monospace',
-  },
-  shadows: {
-    small: '0 0 4px rgba(0, 0, 0, .125)',
-    large: '0 0 24px rgba(0, 0, 0, .125)',
+  button: {
+    border: {
+      radius: '6px',
+    },
+    primary: {
+      color: 'linear-gradient(306.87deg, #4787D3 0%, #67BCFB 100%)',
+    },
+    extend: (props) => `
+      text-transform: uppercase;
+      font-weight: 600;
+    `,
   },
 };
