ef501a7df7dc83f411ad738a068df0062e4507ad
Mate Papp <mate.papp@cogito.study>
2018-11-20 19:58:32 +0100

    Remove console statements from production builds

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+
 import { App } from './app/App';
+import { CONFIG } from './environment/config';
+
+// Allow only error log messages in production mode
+// tslint:disable-next-line
+const empty = () => {};
+if (CONFIG.ENVIRONMENT === 'production') {
+  console.log = empty;
+  console.info = empty;
+  console.debug = empty;
+  console.warn = empty;
+}
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
