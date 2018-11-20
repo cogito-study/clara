73c5ebeca9eb05365a398a1bb0f009ea4068f9fc
Mate Papp <mate.papp@cogito.study>
2018-11-20 19:59:05 +0100

    Create config object and log it in app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,14 +6,20 @@ import { Grommet } from 'grommet';
 import { client } from '../services/client';
 import { theme } from '../ui-kit/theme/theme';
 import { PATH } from '../constants/path';
+import { CONFIG } from '../environment/config';
 import LandingPage from '../landing-page/LandingPage';
 
-export const App = () => (
-  <ApolloProvider client={client}>
-    <Grommet theme={theme}>
-      <Router>
-        <Route exact path={PATH.ROOT} component={LandingPage} />
-      </Router>
-    </Grommet>
-  </ApolloProvider>
-);
+export const App = () => {
+  console.log('environment', CONFIG.ENVIRONMENT);
+  console.log('api url', CONFIG.API_URL);
+
+  return (
+    <ApolloProvider client={client}>
+      <Grommet theme={theme}>
+        <Router>
+          <Route exact path={PATH.ROOT} component={LandingPage} />
+        </Router>
+      </Grommet>
+    </ApolloProvider>
+  );
+};
---
+++ src/environment/config.ts
@@ -0,0 +1,4 @@
+export const CONFIG = {
+  ENVIRONMENT: process.env.REACT_APP_ENVIRONMENT,
+  API_URL: process.env.REACT_APP_API_URL,
+};
