99b7afcaec73f4fd6d8aab55a5dd5f8b769aba3a
Mate Papp <mate.papp@cogito.study>
2018-12-06 20:50:12 +0100

    Finalize structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,13 +2,14 @@ import React, { lazy, Suspense } from 'react';
 import { Link, BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Grommet, Box } from 'grommet';
 
+import { routePath } from '../constants/routePath';
 import { theme } from '../ui/theme/theme';
 import { LoadingPage } from '../pages/LoadingPage';
 
 const RegisterPage = lazy(() => import('../pages/RegisterPage'));
 const SubjectPage = lazy(() => import('../pages/SubjectPage'));
 const NotePage = lazy(() => import('../pages/NotePage'));
-// const LandingPage = lazy(() => import('../landing-page/LandingPage'));
+// const LandingPage = lazy(() => import('../landing-page/LandingPage')); // TODO: Substitute to real landing page
 const GrommetComponents = lazy(() => import('../ui/GrommetComponents'));
 
 export const App = () => (
@@ -16,18 +17,21 @@ export const App = () => (
     <BrowserRouter>
       <Suspense fallback={<LoadingPage />}>
         <Box background="primary" flex="grow" direction="row" justify="between" align="center" wrap>
-          <Link to="/">Landing Page</Link>
-          <Link to="/sote/erseb/info">Subject Info</Link>
-          <Link to="/sote/erseb/notes">Subject Note List</Link>
-          <Link to="/sote/erseb/notes/12">12. Note</Link>
-          <Link to="/sote/erseb/notes/5">5. Note</Link>
+          <Link to={routePath.root}>Landing Page</Link>
+          <Link to={routePath.components}>Components</Link>
+          <Link to={routePath.subjectInfo}>Subject Info</Link>
+          <Link to={routePath.subjectNotes}>Subject Note List</Link>
+          <Link to={routePath.register}>Register</Link>
+          <Link to={`${routePath.register}/asd`}>Register asd</Link>
+          <Link to={`${routePath.subjectNotes}/12`}>12. Note</Link>
+          <Link to={`${routePath.subjectNotes}/5`}>5. Note</Link>
         </Box>
         <Switch>
-          <Route exact path="/" component={(props) => <div {...props}>Landing Page helye</div>} />
-          <Route exact path="/sote/erseb/notes/:noteID" component={(props) => <NotePage {...props} />} />
-          <Route path="/register" component={(props) => <RegisterPage {...props} />} />
-          <Route path="/sote/erseb" component={(props) => <SubjectPage {...props} />} />
-          <Route path="/components" component={(props) => <GrommetComponents {...props} />} />
+          <Route exact path={routePath.root} component={(props) => <div {...props}>Landing Page helye</div>} />
+          <Route exact path={routePath.subjectNoteWithParams} component={(props) => <NotePage {...props} />} />
+          <Route path={routePath.register} component={(props) => <RegisterPage {...props} />} />
+          <Route path={routePath.subject} component={(props) => <SubjectPage {...props} />} />
+          <Route path={routePath.components} component={(props) => <GrommetComponents {...props} />} />
         </Switch>
       </Suspense>
     </BrowserRouter>
