7c5f1c7c6c4bbf074342611e623d24cd665f9903
Mate Papp <mate.papp@cogito.study>
2018-12-08 20:32:48 +0100

    Create SubjectTabs component

--- src/pages/SubjectPage.tsx
+++ src/pages/SubjectPage.tsx
@@ -1,19 +1,14 @@
 import React, { FunctionComponent } from 'react';
-import { Route, Switch, RouteComponentProps } from 'react-router-dom';
+import { RouteComponentProps } from 'react-router-dom';
 import { Box } from 'grommet';
 
-import { HeaderContainer } from '../containers/HeaderContainer';
-import { SubjectInfoContainer } from '../containers/SubjectInfoContainer';
-import { SubjectNoteListContainer } from '../containers/SubjectNoteListContainer';
+import { SubjectHeaderContainer } from '../containers/SubjectHeaderContainer';
+import { SubjectTabsContainer } from '../containers/SubjectTabsContainer';
 
 const SubjectPage: FunctionComponent<RouteComponentProps> = ({ match }) => (
   <Box fill>
-    <HeaderContainer />
-    <Switch>
-      <Route exact path={match.path} render={() => <div>You can not be here</div>} />
-      <Route exact path={`${match.path}/notes`} component={SubjectNoteListContainer} />
-      <Route path={`${match.path}/info`} component={SubjectInfoContainer} />
-    </Switch>
+    <SubjectHeaderContainer />
+    <SubjectTabsContainer />
   </Box>
 );
 
