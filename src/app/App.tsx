5beeb434fde47f9f16cc9519c819fd0d503360b3
Mate Papp <mate.papp@cogito.study>
2018-12-13 21:49:18 +0100

    Refactor registration container using hooks

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import React, { lazy, Suspense } from 'react';
 import { Link, BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
 import { Grommet, Box, Heading } from 'grommet';
 
 import { routePath } from '../constants/routePath';
@@ -16,28 +17,30 @@ const GrommetComponents = lazy(() => import('../ui/GrommetComponents'));
 
 export const App = () => (
   <ApolloProvider client={client}>
-    <Grommet theme={theme} full>
-      <BrowserRouter>
-        <Suspense fallback={<LoadingPage />}>
-          <Box background="primary" flex="grow" direction="row" justify="between" align="center" wrap>
-            <Link to={routePath.root}>Landing Page</Link>
-            <Link to={routePath.components}>Components</Link>
-            <Link to={routePath.subjectInfo}>Subject Info</Link>
-            <Link to={routePath.subjectNotes}>Subject Note List</Link>
-            <Link to={routePath.register}>Register</Link>
-            <Link to={`${routePath.register}/3`}>Register 3</Link>
-            <Link to={`${routePath.subjectNotes}/12`}>12. Note</Link>
-            <Link to={`${routePath.subjectNotes}/5`}>5. Note</Link>
-          </Box>
-          <Switch>
-            <Route exact path={routePath.subjectNoteWithParams} component={(props) => <NotePage {...props} />} />
-            <Route path={routePath.register} component={(props) => <RegisterPage {...props} />} />
-            <Route path={routePath.subject} component={(props) => <SubjectPage {...props} />} />
-            <Route path={routePath.components} component={(props) => <GrommetComponents {...props} />} />
-            <Route path={routePath.root} component={(props) => <Heading {...props}>Landing Page helye</Heading>} />
-          </Switch>
-        </Suspense>
-      </BrowserRouter>
-    </Grommet>
+    <ApolloHooksProvider client={client}>
+      <Grommet theme={theme} full>
+        <BrowserRouter>
+          <Suspense fallback={<LoadingPage />}>
+            <Box background="primary" flex="grow" direction="row" justify="between" align="center" wrap>
+              <Link to={routePath.root}>Landing Page</Link>
+              <Link to={routePath.components}>Components</Link>
+              <Link to={routePath.subjectInfo}>Subject Info</Link>
+              <Link to={routePath.subjectNotes}>Subject Note List</Link>
+              <Link to={routePath.register}>Register</Link>
+              <Link to={`${routePath.register}/3`}>Register 3</Link>
+              <Link to={`${routePath.subjectNotes}/12`}>12. Note</Link>
+              <Link to={`${routePath.subjectNotes}/5`}>5. Note</Link>
+            </Box>
+            <Switch>
+              <Route exact path={routePath.subjectNoteWithParams} component={(props) => <NotePage {...props} />} />
+              <Route path={routePath.register} component={(props) => <RegisterPage {...props} />} />
+              <Route path={routePath.subject} component={(props) => <SubjectPage {...props} />} />
+              <Route path={routePath.components} component={(props) => <GrommetComponents {...props} />} />
+              <Route path={routePath.root} component={(props) => <Heading {...props}>Landing Page helye</Heading>} />
+            </Switch>
+          </Suspense>
+        </BrowserRouter>
+      </Grommet>
+    </ApolloHooksProvider>
   </ApolloProvider>
 );
