866db2383d76e4ae4d60879f25125c9e047c2c99
Mate Papp <mate.papp@cogito.study>
2018-12-20 12:04:54 +0100

    Import apollo-error-link

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,6 +1,7 @@
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
+import { onError } from 'apollo-link-error';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import { config } from '../environment/config';
@@ -18,7 +19,27 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const errorLink = onError(({ graphQLErrors, networkError, operation, response }) => {
+  if (operation) {
+    console.log(`[Operation]: ${operation}`);
+  }
+
+  if (response) {
+    console.log(`[Response]: ${response}`);
+  }
+
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`),
+    );
+  }
+
+  if (networkError) {
+    console.log(`[Network error]: ${networkError}`);
+  }
+});
+
 export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: authLink.concat(errorLink).concat(httpLink),
   cache: new InMemoryCache(),
 });
