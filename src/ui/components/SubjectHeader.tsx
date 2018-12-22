81490047a80af5892ad7d593ea83d87578b24bd8
Adam Lobler <adam.lobler@cogito.study>
2018-12-22 15:42:18 +0100

    Beta sign added to header

--- src/ui/components/SubjectHeader.tsx
+++ src/ui/components/SubjectHeader.tsx
@@ -1,8 +1,8 @@
 import React, { FunctionComponent } from 'react';
-import { Box, Heading, Image, Button } from 'grommet';
+import { Box, Heading, Image, Button, Text } from 'grommet';
 import { Logout } from 'grommet-icons';
-
 import logo from '../../assets/images/logo.svg';
+import styled from 'styled-components';
 
 interface Props {
   title: string;
@@ -10,10 +10,31 @@ interface Props {
   onLogout: () => void;
 }
 
+const BetaBox = styled(Box)`
+  min-width: 45px;
+`;
+
 export const SubjectHeader: FunctionComponent<Props> = ({ title, userName, onLogout }) => (
   <Box flex width="xlarge" height="60px" pad="xsmall" justify="between" align="center" direction="row">
-    <Box basis="1/4" justify="center">
+    <Box basis="1/4" justify="center" align="center" direction="row">
       <Image src={logo} width="150px" />
+      <BetaBox
+        margin={{ horizontal: 'medium', top: '4px', bottom: 'none' }}
+        height="20px"
+        round="xsmall"
+        background="white"
+      >
+        <Text
+          textAlign="center"
+          alignSelf="center"
+          size="11px"
+          weight="bold"
+          margin={{ horizontal: 'xsmall', top: '3px', bottom: 'none' }}
+          color="primary"
+        >
+          BETA
+        </Text>
+      </BetaBox>
     </Box>
     <Box basis="1/2">
       <Heading level="2" color="white" textAlign="center">
