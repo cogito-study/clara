de5cbef7f90081376ff1ab02303ca51a79920414
Adam Lobler <adam.lobler@cogito.study>
2018-12-28 19:34:46 +0100

    Updated NoteComment

--- src/ui/components/NoteComment.tsx
+++ src/ui/components/NoteComment.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Heading, Image, Text } from 'grommet';
 import React, { FunctionComponent } from 'react';
 import styled from 'styled-components';
 import upvote from '../../assets/images/Upvote.svg';
+import trash from '../../assets/images/Trash.svg';
 import upvoted from '../../assets/images/Upvoted.svg';
 
 export interface Props {
@@ -11,78 +12,103 @@ export interface Props {
   upvoteCounts: number;
   isUpvoted: boolean;
   onVote: (isUpvoted: boolean) => void;
+  onDelete?: () => void;
 }
 
-const HoveredBox = styled(Box)`
+/*
+TODO: Make individual button components
+*/
+
+const UpvoteHoveredBox = styled(Box)`
   transition: all 0.2s ease-in-out;
   max-height: 500px;
-  border-bottom-left-radius: 12px;
-  border-bottom-right-radius: 12px;
+  border-radius: 20px;
+  box-shadow: inset 0px 0px 0px 1px #4787d3;
   &:hover {
-    box-shadow: inset 0px 0px 0px 1.5px #4787d3;
+    box-shadow: inset 0px 0px 0px 2px #4787d3;
+  } /*  */
+`;
+
+const DeleteHoveredBox = styled(Box)`
+  transition: all 0.2s ease-in-out;
+  max-height: 500px;
+  border-radius: 20px;
+  box-shadow: inset 0px 0px 0px 1px #ff2b5e;
+  &:hover {
+    box-shadow: inset 0px 0px 0px 2px #ff2b5e;
   } /*  */
 `;
 
 export const NoteComment: FunctionComponent<Props> = ({ author, date, paragraph, upvoteCounts, isUpvoted, onVote }) => {
   return (
-    <Box width="320px" background="white" direction="column" round="small" overflow="hidden" elevation="medium">
-      <Box fill pad="medium">
-        <Box fill background="white" direction="row" align="center" justify="between" margin={{ bottom: 'medium' }}>
+    <Box width="320px" background="white" direction="column" round="20px" overflow="hidden" elevation="medium">
+      <Box fill pad={{ bottom: 'small', horizontal: 'medium', top: 'medium' }}>
+        <Box fill background="white" direction="row" align="center" justify="between" margin={{ bottom: 'small' }}>
           <Box direction="column">
             <Heading level="3" margin="none">
               {author}
             </Heading>
-            <Text color="gray" size="small" margin="none">
+            <Text color="gray" size="xsmall" margin="none">
               {date}
             </Text>
           </Box>
-          <Box direction="row">
-            <Text
-              margin="xsmall"
-              size="18px"
-              weight={isUpvoted ? 'bold' : 'normal'}
-              color={'primary'}
-              alignSelf="center"
-            >
-              {upvoteCounts}
-            </Text>
-            <Image src={upvote} width="24px" margin={{ bottom: '4px' }} />
-          </Box>
         </Box>
-        <Text size="16px">{paragraph}</Text>
+        <Text size="14px">{paragraph}</Text>
       </Box>
-      <Box background="backgroundBlue" height="2px" />
-      <HoveredBox
-        fill
-        direction="column"
-        align="center"
-        justify="center"
-        background={isUpvoted ? 'primary' : 'white'}
-        width="50px"
-        overflow="hidden"
-        pad="none"
-      >
-        <Button
-          fill
-          plain
-          reverse
-          icon={<Image src={isUpvoted ? upvoted : upvote} width="21px" margin={{ bottom: '4px' }} />}
-          margin="10px"
-          label={
-            <Text
-              margin="none"
-              size="16px"
-              weight={isUpvoted ? 'bold' : 'normal'}
-              color={isUpvoted ? 'white' : 'primary'}
-              alignSelf="center"
-            >
-              {isUpvoted ? 'UPVOTED' : 'UPVOTE'}
-            </Text>
-          }
-          color={`${isUpvoted}` ? 'primary' : 'primary'}
-          onClick={() => onVote(isUpvoted)}
-        />
-      </HoveredBox>
+      <Box justify="end" direction="row" pad={{ horizontal: 'small', vertical: 'small' }}>
+        <DeleteHoveredBox
+          direction="column"
+          align="center"
+          margin={{ horizontal: '5px' }}
+          justify="center"
+          background="white"
+          overflow="hidden"
+          pad={{ horizontal: '10px', vertical: '0px' }}
+        >
+          <Button
+            plain
+            reverse
+            alignSelf="center"
+            icon={
+              <Image
+                src={trash}
+                width="16px"
+                margin={{ left: '-10px' /*because of the empty label..*/, bottom: '2px' }}
+              />
+            }
+            margin={{ horizontal: '2px', vertical: '6px' }}
+            label={<Text size="15px" alignSelf="center" />} // if you delete the label, it makes a 12px wide padding around the icon...
+          />
+        </DeleteHoveredBox>
+        <UpvoteHoveredBox
+          direction="column"
+          align="center"
+          justify="center"
+          background={isUpvoted ? 'primary' : 'white'}
+          overflow="hidden"
+          margin={{ horizontal: '5px' }}
+          pad={{ horizontal: '10px', vertical: '0px' }}
+        >
+          <Button
+            plain
+            reverse
+            icon={<Image src={isUpvoted ? upvoted : upvote} width="16px" margin={{ bottom: '2px' }} />}
+            margin={{ horizontal: '2px', vertical: '6px' }}
+            label={
+              <Text
+                margin={{ right: '-8px' }}
+                size="15px"
+                weight={isUpvoted ? 'bold' : 'normal'}
+                color={isUpvoted ? 'white' : 'primary'}
+                alignSelf="center"
+              >
+                {upvoteCounts}{' '}
+              </Text>
+            }
+            onClick={() => onVote(isUpvoted)}
+          />
+        </UpvoteHoveredBox>
+      </Box>
     </Box>
   );
 };
