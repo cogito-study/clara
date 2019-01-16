b969f209192ccaeb7646ad23d366ccebcf7d094d
Bertalan Körmendy <korber314@gmail.com>
2019-01-16 19:31:22 +0100

    fixed drag-drop bug

--- src/editor/Editor.tsx
+++ src/editor/Editor.tsx
@@ -73,6 +73,7 @@ export default class Editor extends PureComponent<Props, State> {
         unwrapCommand: unwrapLink,
       }),
       CollapseOnEscape(),
+      ReadOnlyPlugin(),
     ];
   }
 
--- src/editor/plugins/ReadOnlyPlugin.tsx
+++ src/editor/plugins/ReadOnlyPlugin.tsx
@@ -1,6 +1,14 @@
 import { Editor as CoreEditor } from 'slate';
 
 export const ReadOnlyPlugin = () => ({
+  onBeforeInput(event: Event, editor: CoreEditor, next: VoidFunction) {
+    event.preventDefault();
+    return editor;
+  },
+  onDrop(event: Event, editor: CoreEditor, next: VoidFunction) {
+    event.preventDefault();
+    return editor;
+  },
   onCommand(command: any, editor: CoreEditor, next: VoidFunction) {
     return next();
   },
