976f7c48b17765b2c9a60926b8fa7c8060f02035
balint.kiraly <balint.kiraly@cogito.study>
2019-02-25 18:36:00 +0100

    Drift chatbox stayed closed

--- src/scripts/drift.ts
+++ src/scripts/drift.ts
@@ -2,6 +2,7 @@ export const driftString = `"use strict";
 
 ! function () {
   var t = window.driftt = window.drift = window.driftt || [];
+
   if (!t.init) {
     if (t.invoked) return void(window.console && console.error && console.error("Drift snippet included twice."));
     t.invoked = !0, t.methods = ["identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on"],
@@ -24,6 +25,12 @@ export const driftString = `"use strict";
     document.querySelector('#drift-widget-container').hidden = false;
   }
 }();
+drift.config({
+  messages: {
+    welcomeMessage: 'Segíthetünk? Ha kérdésed van, írj bátran!'
+   },
+   enableCampaigns: false
+});
 drift.SNIPPET_VERSION = '0.3.1';
 drift.load('dzw4g36p7k4z');
 `;
