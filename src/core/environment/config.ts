ddf46209abd4c7a44ec6c2add837835ff9133222
Mate Papp <mate.papp@cogito.study>
2019-10-15 17:04:24 +0000

    Remove env

--- src/core/environment/config.ts
+++ src/core/environment/config.ts
@@ -3,6 +3,8 @@ export const config = {
   apiURL: process.env.REACT_APP_API_URL,
   googleAnalyticsKey: 'UA-135440672-1',
   sentryDSN: 'https://fb58dd3770e24645ae9023bbd5797c7c@sentry.io/1363186',
+
+  // TODO: Revise terms and privacy URL usage
   termsURL: process.env.PUBLIC_URL + '/documents/ASZF.pdf',
   privacyURL: process.env.PUBLIC_URL + '/documents/Adatvedelem.pdf',
 };
