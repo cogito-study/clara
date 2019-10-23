9c426efcfc9e8c5d3e7fbf23dbe4e3c5ac1c8975
Kristóf Kovács-Egri <kristof.kovacsegri@cogito.study>
2019-10-23 14:45:50 +0200

    Header component created on mobile and desktop size

--- src/collab/components/editor/editor.stories.tsx
+++ src/collab/components/editor/editor.stories.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import { collabComponents } from '../../utils/storybook';
+import { EditorHeader, EditorHeaderProps } from './editor-header';
 
 export default {
   title: collabComponents('Editor'),
 };
 
-export const placeholder = () => <div />;
+const editorHeaderPropsInEditMode: EditorHeaderProps = {
+  mode: 'edit',
+  subject: 'Vascular surgery',
+};
+
+const editorHeaderPropsInStudyMode: EditorHeaderProps = {
+  mode: 'study',
+  subject: 'Vascular surgery',
+};
+
+export const editorHeaderInEditMode = () => <EditorHeader {...editorHeaderPropsInEditMode} />;
+export const editorHeaderWhenInStudyMode = () => <EditorHeader {...editorHeaderPropsInStudyMode} />;
