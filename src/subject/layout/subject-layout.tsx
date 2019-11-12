bdf644d5c41088d6d2b9f7be7c9d3f13373e5cd7
Kristóf Kovács-Egri <kristof.kovacsegri@cogito.study>
2019-11-12 12:13:09 +0000

    Fix subject name on menu and tabs alignment along with subject info paddings

--- src/subject/layout/subject-layout.tsx
+++ src/subject/layout/subject-layout.tsx
@@ -1,13 +1,23 @@
 import { Flex } from '@chakra-ui/core';
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import { MainMenu } from '../../core/components/navigation/menu';
 import { SubjectPage } from '../pages/subject-page';
+import { SubjectRouteParams } from '../utils/subject-route';
+import { useSubjectPageQuery } from './graphql/subject-page-query.generated';
 
 export const SubjectLayout = () => {
+  const { subjectCode } = useParams<SubjectRouteParams>();
+  const { data } = useSubjectPageQuery({ variables: { subjectCode } });
+
   return (
     <Flex direction={['column', 'column', 'row']} bg="white">
-      <MainMenu />
-      <SubjectPage />
+      {data && data.subject && data.subject.name && data.subject.name && (
+        <>
+          <MainMenu subjectTitle={data.subject.name} />
+          <SubjectPage subjectTitle={data.subject.name} />
+        </>
+      )}
     </Flex>
   );
 };
