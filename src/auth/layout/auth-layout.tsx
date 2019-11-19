740d458c6ce1e9b43cda634c7f4a83bf6108033b
Kristóf Kovács-Egri <kristof.kovacsegri@cogito.study>
2019-11-19 17:11:29 +0000

    Cogito logo and background added to auth components

--- src/auth/layout/auth-layout.tsx
+++ src/auth/layout/auth-layout.tsx
@@ -1,14 +1,28 @@
+import { Box, Flex, Icon } from '@chakra-ui/core';
 import React, { FC } from 'react';
-import { Box, Flex } from '@chakra-ui/core';
+import background from '../assets/auth-bg.svg';
 
-// TODO: Logo and pattern background
 export const AuthLayout: FC = ({ children }) => {
   return (
     <Flex direction="column" h="100vh">
       <Flex bg="#fff" borderColor="blue.100" borderWidth={1} h={16} align="center" justify="center">
-        Cogito
+        <Icon
+          color="blue.800"
+          size="80px"
+          //@ts-ignore
+          name="cogito-with-text"
+        />
       </Flex>
-      <Flex bg="red.200" flex="1" align={['start', 'start', 'center']} justify="center" p={4}>
+      <Flex
+        bg="white"
+        backgroundImage={`url(${background})`}
+        backgroundPosition={['center left', 'center left', 'center']}
+        backgroundRepeat="repeat"
+        flex="1"
+        align={['start', 'start', 'center']}
+        justify="center"
+        p={4}
+      >
         <Box maxW="400px" flex="1">
           {children}
         </Box>
